411c026971e00c4392a861e1462d23212304e299
ikutarian <saitou0217@outlook.com>
2018-10-20 13:04:52 +0800

    时间戳优化

---
+++ src/main/java/com/ikutarian/mmall/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.ikutarian.mmall.dao;
+
+import com.ikutarian.mmall.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
---
+++ src/main/java/com/ikutarian/mmall/dao/ShippingMapper.java
@@ -0,0 +1,17 @@
+package com.ikutarian.mmall.dao;
+
+import com.ikutarian.mmall.pojo.Shipping;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
